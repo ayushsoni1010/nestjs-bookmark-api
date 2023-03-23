79517cf71a59a333ada3898ac73d523190cc4cac
Ayush Soni <ayushsoni1010.work@gmail.com>
2023-03-23 16:26:58 +0530

    feat: jwt strategy

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,29 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PrismaService } from '../../prisma/prisma.service';
+import { ConfigService } from '@nestjs/config';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(config: ConfigService, private prisma: PrismaService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.get('JWT_SECRET'),
+    });
+  }
+  async validate(payload: { sub: number; email: string }) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: payload.sub,
+        email: payload.email,
+      },
+    });
+    delete user.hash;
+
+    if (!user) {
+      throw new ForbiddenException('User not found and not validated');
+    }
+    return user;
+  }
+}
